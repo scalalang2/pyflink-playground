a0cf92998221470b9cf6e552740298887743fe48
scalalang2 <cailhuiris@gmail.com>
2023-05-10 15:25:26 +0900

    I see it works !

--- jobs/kafka-to-s3/main.py
+++ jobs/kafka-to-s3/main.py
@@ -11,24 +11,22 @@ from pyflink.table import StreamTableEnvironment
 env = StreamExecutionEnvironment.get_execution_environment()
 t_env = StreamTableEnvironment.create(stream_execution_environment=env)
 
-is_local = True if os.environ.get("IS_LOCAL") else False
-if is_local:
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--dep',
-        dest='dep',
-        required=True)
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--dep',
+    dest='dep',
+    required=True)
 
-    argv = sys.argv[1:]
-    known_args, _ = parser.parse_known_args(argv)
-    t_env.get_config().set("pipeline.jars", known_args.dep)
+argv = sys.argv[1:]
+known_args, _ = parser.parse_known_args(argv)
+t_env.get_config().set("pipeline.jars", known_args.dep)
 
-    t_env.get_config().get_configuration().set_string(
-        "execution.checkpointing.mode", "EXACTLY_ONCE"
-    )
-    t_env.get_config().get_configuration().set_string(
-        "execution.checkpointing.interval", "1min"
-    )
+t_env.get_config().get_configuration().set_string(
+    "execution.checkpointing.mode", "EXACTLY_ONCE"
+)
+t_env.get_config().get_configuration().set_string(
+    "execution.checkpointing.interval", "1min"
+)
 
 def create_source_table(table_name, broker):
     return """ CREATE TABLE {0} (
@@ -42,6 +40,7 @@ def create_source_table(table_name, broker):
         'topic' = 'bank_transfers',
         'properties.group.id' = 'test_group',
         'properties.bootstrap.servers' = '{1}',
+        'scan.startup.mode' = 'earliest-offset',
         'format' = 'json'
     )""".format(table_name, broker)
 
